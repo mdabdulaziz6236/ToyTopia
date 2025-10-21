7cae76b00c44f53d4ecd9c85785316e76ca6d511
mdabdulaziz6236 <md.abdulaziz6236@gmail.com>
2025-10-22 00:39:06 +0600

    In the Name of Allah, started project downloaded with tailwindcss, daisyUi, react router, react-toastify,and also firebase . added favicon and added some components and file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Bismillahir Rahmanir Rahim. Salli a'lal Habib wa Barik wa Sallam</h1>
+    </>
+  );
+}
+
+export default App;
